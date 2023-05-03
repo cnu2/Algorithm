387ebcce9b94fe1e16ac04d89f67b1c3f61495b7
cnu2 <leecw94u@gmail.com>
2023-05-03 17:50:37 +0900

    first commit

---
+++ Simul_10817.py
@@ -0,0 +1,34 @@
+"""
+1/5
+1. 23/05/03
+2. 
+3.
+4.
+5.
+"""
+
+"""
+[문제]
+세 정수 A, B, C가 주어진다. 이때, 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오. 
+
+"""
+
+"""
+[문제 해결]
+숫자를 입력받고
+sort을 진행하고
+1번째 index를 반환한다.
+"""
+
+"""
+[sudo code]
+list = list(input())
+list.sort
+print(list[1])
+
+"""
+import sys
+num_list = list(map(int,sys.stdin.readline().split()))
+num_list.sort()
+breakpoint()
+print(num_list[1])
