590e48397ec492f0e3d70070f111741771126847
cnu2 <leecw94u@gmail.com>
2023-05-17 10:37:48 +0900

    first commit

---
+++ BFS_2667.py
@@ -0,0 +1,84 @@
+"""
+1/3
+1. 23/05/16
+2. 
+3.
+"""
+
+"""
+[문제]
+<그림 1>과 같이 정사각형 모양의 지도가 있다. 1은 집이 있는 곳을, 0은 집이 없는 곳을 나타낸다. 철수는 이 지도를 가지고 연결된 집의 모임인 단지를 정의하고, 단지에 번호를 붙이려 한다. 
+여기서 연결되었다는 것은 어떤 집이 좌우, 혹은 아래위로 다른 집이 있는 경우를 말한다. 
+대각선상에 집이 있는 경우는 연결된 것이 아니다. <그림 2>는 <그림 1>을 단지별로 번호를 붙인 것이다. 지도를 입력하여 단지수를 출력하고, 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력하는 프로그램을 작성하시오.
+"""
+
+"""
+[sudo code]
+1. n 입력
+2. map 입력
+3. 상하좌우로 가는 방향벡터 d 설정
+4. 원소를 돌면서 원소의 값이 1이라면
+5. 그 위치를 queue에 저장
+6. while queue:
+    그 위치를 기준으로 상하좌우로 움직인다.
+    해당 위치는 0으로 변환 
+    상하좌우로 움직인 곳이 1이라면
+        queue에 append
+    count +=1
+    return count
+
+7. count 값을 result에 append
+5. result를 오름차순으로 sort
+6. print(result)
+    
+
+
+"""
+from collections import deque
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+
+def bfs(graph, a, b):
+    n = len(graph)
+    queue = deque()
+    queue.append((a, b))
+    graph[a][b] = 0
+    count = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx, ny))
+                count += 1
+    return count
+
+
+n = int(input())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+cnt = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            cnt.append(bfs(graph, i, j))
+
+cnt.sort()
+print(len(cnt))
+for i in range(len(cnt)):
+    print(cnt[i])
+
+
+
+
+
+
