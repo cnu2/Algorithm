3c4ebc2bc2cd4a9a5f168b5d54eb9f35d829103c
Chanwoo Lee <leecw94u@gmail.com>
2024-02-06 18:06:17 +0900

    greedy, using hint

--- BOJ_11497.py
+++ BOJ_11497.py
@@ -1,35 +1,3 @@
-# import sys
-# input = sys.stdin.readline
-# testCase = int(input()) # 테스트 케이스
-# answer = []
-# for _ in range(testCase):
-#     ans = 0
-#     n = int(input()) # 통나무의 개수
-#     evens = []
-#     odds = []
-#     woods = list(map(int, input().split())) # 통나무의 높이
-#     for i in range(n):
-#         if i % 2 == 0:
-#             if woods[i] in evens:
-#                 odds.append(woods[i])
-#             else:
-#                 evens.append(woods[i])
-#         else:
-#             if woods[i] in odds:
-#                 evens.append(woods[i])
-#             else:
-#                 odds.append(woods[i])
-#     evens = sorted(evens)
-#     odds = sorted(odds, reverse=True)
-#     result = evens + odds
-#     for i in range(n):
-#         temp = abs(result[i]-result[i-1])
-#         ans = max(ans, temp)
-#     answer.append(ans)
-
-# for i in answer:
-#     print(i)
-
 import sys
 input = sys.stdin.readline
 testCase = int(input()) # 테스트 케이스
@@ -44,7 +12,7 @@ for _ in range(testCase):
         if i % 2 == 0:
             left.append(woods[i])
         else:
-            right.insert(0,woods[i])
+            right.insert(0,woods[i]) # 힌트이용
     result = left + right 
     for i in range(n):
         temp = abs(result[i]-result[i-1])
