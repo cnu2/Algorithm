876ab6a58b4b470141ed1d5639ed341b7b7a902f
Chanwoo Lee <leecw94u@gmail.com>
2024-03-10 23:27:00 +0900

    simulation

--- BOJ_17952.py
+++ BOJ_17952.py
@@ -12,10 +12,15 @@ for _ in range(n):
         pass
     else:
         time, score, duration = temp[0], temp[1], temp[2]
-        work = [[score,duration] + work]
-    work[0][1] -= 1
-    if work[0][1] == 0:
-        answer += work[0][0]
-        del(work[0])
+        work.append([score, duration])
 
+    if len(work) == 0:
+        continue
+    work[-1][1] -= 1
+    if work[-1][1] == 0:
+        answer += work[-1][0]
+        if len(work) == 1:
+            work = []
+        else:
+            work = work[:-1]
 print(answer)
