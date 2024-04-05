484076045bc2093386b55ef6c34503e6cd11caad
Chanwoo Lee <leecw94u@gmail.com>
2024-04-05 16:27:32 +0900

    이분탐색,lis,다시풀기 完(4/5)

--- BOJ_14002.py
+++ BOJ_14002.py
@@ -9,60 +9,41 @@
 # print(bisect.bisect_left(inc_arr, sys.maxsize))
 # print(inc_arr)
 
-# import sys
-# input = sys.stdin.readline
-
-# n = int(input())
-# arr = list(map(int, input().split()))
-# inc_arr = [arr[0]]
-
-# def binary_search(left, right, target):
-#     while left <= right:
-#         mid = (left + right)//2
-#         if inc_arr[mid] == target:
-#             return mid
-#         elif inc_arr[mid] > target:
-#             right = mid - 1
-#         else:
-#             left = mid + 1
-#     return left
-
-# for i in range(1,n):
-#     if arr[i] > inc_arr[-1]:
-#         inc_arr.append(arr[i])
-#     else:
-#         idx = binary_search(0, len(inc_arr)-1, arr[i])
-#         inc_arr[idx] = arr[i]
-
-# print(len(inc_arr))
-# print(' '.join(map(str, inc_arr)))
-
 import sys
-from bisect import bisect_left
 input = sys.stdin.readline
+INF = sys.maxsize
 
-N = int(input())
-A = list(map(int, input().split()))
+n = int(input())
+arr = list(map(int, input().split()))
+inc_arr = [arr[0]]
+dist = [-INF]
+index = [0] * (n + 1)
+answer = []
 
-dist = [-1000000001]
-index = [0] * (N + 1)
+def binary_search(left, right, target):
+    while left <= right:
+        mid = (left + right)//2
+        if dist[mid] == target:
+            return mid
+        elif dist[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
 
-for i in range(len(A)):
-    num = A[i]
-    if dist[-1] < num:
-        dist.append(num)
+for i in range(n):
+    if arr[i] > dist[-1]:
+        dist.append(arr[i])
         index[i] = len(dist) - 1
     else:
-        index[i] = bisect_left(dist, num)
-        dist[index[i]] = num
-    breakpoint()
+        index[i] = binary_search(0, len(dist)-1, arr[i])
+        dist[index[i]] = arr[i]
 
 max_length = len(dist) - 1
 print(max_length)
-answer = []
-for i in range(N, -1, -1):
+for i in range(n, -1, -1):
     if index[i] == max_length:
-        answer.append(A[i])
+        answer.append(arr[i])
         max_length -= 1
-
 print(*(reversed(answer)))
+
